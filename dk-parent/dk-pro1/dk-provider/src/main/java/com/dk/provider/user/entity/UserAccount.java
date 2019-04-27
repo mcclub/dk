7741d684d3239a82ab06e8e132b181f9f5bf160f
MCjinxin <1136092840@qq.com>
2019-04-27 14:50:45 +0800

    生成user,user_account 类实体

---
+++ dk-parent/dk-pro1/dk-provider/src/main/java/com/dk/provider/user/entity/UserAccount.java
@@ -0,0 +1,115 @@
+package com.dk.provider.user.entity;
+
+import java.util.Date;
+
+public class UserAccount {
+    private Long id;
+
+    private Long userId;
+
+    private String balance;
+
+    private String rokeAmt;
+
+    private String password;
+
+    private Long states;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private String createBy;
+
+    private String updateBy;
+
+    private Long isDelete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance == null ? null : balance.trim();
+    }
+
+    public String getRokeAmt() {
+        return rokeAmt;
+    }
+
+    public void setRokeAmt(String rokeAmt) {
+        this.rokeAmt = rokeAmt == null ? null : rokeAmt.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Long getStates() {
+        return states;
+    }
+
+    public void setStates(Long states) {
+        this.states = states;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public Long getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Long isDelete) {
+        this.isDelete = isDelete;
+    }
+}
---
+++ dk-parent/dk-pro1/dk-provider/src/main/java/com/dk/provider/user/mapper/UserAccountMapper.java
@@ -0,0 +1,17 @@
+package com.dk.provider.user.mapper;
+
+import com.dk.provider.user.entity.UserAccount;
+
+public interface UserAccountMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserAccount record);
+
+    int insertSelective(UserAccount record);
+
+    UserAccount selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserAccount record);
+
+    int updateByPrimaryKey(UserAccount record);
+}
