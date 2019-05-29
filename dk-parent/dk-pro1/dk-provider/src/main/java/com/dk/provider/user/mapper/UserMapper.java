039839f74ef4470648732c33eee4fdd8079a1513
MCjinxin <1136092840@qq.com>
2019-05-29 20:31:04 +0800

    合并更新0529

--- dk-parent/dk-pro1/dk-provider/src/main/java/com/dk/provider/user/mapper/UserMapper.java
+++ dk-parent/dk-pro1/dk-provider/src/main/java/com/dk/provider/user/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.dk.provider.user.mapper;
 
 import com.dk.provider.basis.mapper.BaseMapper;
 import com.dk.provider.user.entity.User;
+import com.dk.provider.user.entity.UserFriend;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
@@ -22,5 +24,31 @@ public interface UserMapper extends BaseMapper<User> {
 
     int updatePassword (Map map);
 
+    UserFriend searchFriendList (Map map);
+
+    User searchUserById(Map map);
+
     int udpOpautopay(Map map);
+
+    /**
+     * 查询普通用户下的二级id
+     * @param map
+     * @return
+     */
+    List<Long> searchTwoId(Map map);
+
+    /**
+     * 查询代理商用户下的二级三级id
+     * @param map
+     * @return
+     */
+    List<Long> searchTwoThreeId(Map map);
+
+    /**
+     * 查询代理商用户下的三级id
+     * @param map
+     * @return
+     */
+    List<Long> searchThreeId(Map map);
+
 }
